27292dbc8e2c03191a461a7aef650a5b548d7661
skhalsa-sigsci <skhalsa-sigsci@users.noreply.github.com>
2022-08-25 14:32:51 -0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/skhalsa-sigsci/websocket-chat
+
+go 1.18
+
+require github.com/gorilla/websocket v1.5.0
---
+++ server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{} // use default options
+
+func ws(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/ws", ws)
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
